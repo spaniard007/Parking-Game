65afca34cc5c4cc1e7070fedb4a6364362a06a93
spaniard007 <46812942+spaniard007@users.noreply.github.com>
2022-07-21 19:52:29 +0600

    --- Level Designing finished
    --- car added
    --- Touch Controller started

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchInput : MonoBehaviour,IPointerDownHandler,IPointerUpHandler,IDragHandler
+{
+
+    private bool isMousePressed;
+    private DragData dragData;
+
+    public int raycastDistance;
+    public int dragThreshold;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Ray ray = CameraController.instance.mainCamera.ScreenPointToRay(eventData.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, raycastDistance, 256))
+        {
+            VehicleMovementController vehicle = hit.transform.GetComponent<VehicleMovementController>();
+            if (vehicle != null)
+            {
+                isMousePressed = true;
+                dragData = new DragData{touchPos=eventData.position,vehicle = vehicle};
+            }
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isMousePressed = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(!isMousePressed)
+            return;
+
+        Vector2 dragVal = dragData.touchPos - eventData.position;
+
+        if (dragVal.magnitude > dragThreshold)
+        {
+            //attempt vehichle Movement
+        }
+    }
+    
+    private struct DragData
+    {
+        public Vector2 touchPos;
+        public VehicleMovementController vehicle;
+    }
+}
